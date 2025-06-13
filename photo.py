9500152338326e6a5b0f3e804c4b82c632cc7496
kCaCo1 <237917611@qq.com>
2025-06-13 09:30:29 +0800

    Update photo.py

--- photo.py
+++ photo.py
@@ -22,7 +22,9 @@ image_obj = [{
 
 st.image(image_obj[st.session_state['ind']]['url'], caption=image_obj[st.session_state['ind']]['title'])
 
-# 显示按钮
+def prevImg():
+    # 点击上一张按钮要做的事
+    st.session_state['ind'] = (st.session_state['ind'] - 1) % len(image_obj)
 
 def nextImg():
     # 点击下一张按钮要做的事
@@ -31,7 +33,7 @@ def nextImg():
 c1, c2 = st.columns(2)
 
 with c1:
-    st.button('上一张', use_container_width=True)
+    st.button('上一张', onclick=prevImg, use_container_width=True)
 
 with c2:
     st.button('下一张', on_click=nextImg, use_container_width=True)
